ca62d8540dc389eec93b8cfa2bd285fb4a97b2c3
Naveen Bishnoi <naveendharnia0029@gmail.com>
2020-09-18 16:15:56 +0530

    Express Simple Rest

--- NodeJs/Node-express/index.js
+++ NodeJs/Node-express/index.js
@@ -7,15 +7,56 @@ const express=require('express');
  const app=express();
  const morgan=require('morgan');
 
+ const bodyParser=require('body-parser');
+ app.use(bodyParser.json());
+
+
  app.use(morgan('dev'));
  app.use(express.static(__dirname+'/public'));
 
- app.use((req,res,next)=>{
-     console.log(req.headers);
+ app.all('/dishes',(req,res,next)=>{
      res.statusCode=200;
-     res.setHeader('Content-Type','text/html');
-     res.end('<html><body><h1>This is an Express Server</h1></body>');
+     res.setHeader('Content-Type','text/plain');
+     next();
+
  });
+
+ app.get('/dishes',(req,res,next)=>{
+     res.end('will send all dishes to you!');
+ });
+ 
+ app.post('/dishes',(req,res,next)=>{
+     res.end('will add the dish' + req.body.name + 'with details' + req.body.description);
+
+ });
+
+ app.put('/dishes',(req,res,next)=>{
+     res.statusCode=403;
+     res.end('put not supported on /dishes');
+ });
+
+ app.delete('/dishes',(req,res,next) => {
+    res.end('deleting all dishes!');
+ });
+
+ app.get('/dishes/:dishId', (req,res,next) => {
+    res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
+});
+
+app.post('/dishes/:dishId', (req, res, next) => {
+  res.statusCode = 403;
+  res.end('POST operation not supported on /dishes/'+ req.params.dishId);
+});
+
+app.put('/dishes/:dishId', (req, res, next) => {
+  res.write('Updating the dish: ' + req.params.dishId + '\n');
+  res.end('Will update the dish: ' + req.body.name + 
+        ' with details: ' + req.body.description);
+});
+
+app.delete('/dishes/:dishId', (req, res, next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
+});
 
  const server=http.createServer(app);
 
