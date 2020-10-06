6aaa49facd3d287fd299ce66f37c2a2d230392ec
Naveen Bishnoi <naveendharnia0029@gmail.com>
2020-10-06 15:42:07 +0530

    Node callback hell and promises

--- index.js
+++ index.js
@@ -6,30 +6,37 @@ const url='mongodb://localhost:27017/';
 const dbname='conFusion';
 
 
-MongoClient.connect(url,(err,client)=>{
-    assert.equal(err,null);
+MongoClient.connect(url,{useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }).then((client)=>{
+   // assert.equal(err,null);
 const db=client.db(dbname);
 
-dboper.insertDocument(db,{name:"vadonut",description:"Test"},"dishes",(result)=>{
+dboper.insertDocument(db,{name:"vadonut",description:"Test"},"dishes")
+.then((result)=>{
      console.log('Insert Document:\n',result.ops);
      
-     dboper.findDocument(db,"dishes",(docs)=>{
+     return dboper.findDocument(db,"dishes")
+})
+.then((docs)=>{
          console.log("Find Documets:\n",docs);
 
-         dboper.updateDocument(db,{name:"vadonut",},{description:"updated test"},"dishes",(result)=>{
+         return dboper.updateDocument(db,{name:"vadonut",},{description:"updated test"},"dishes")
+ })
+.then((result)=>{
              console.log('Updated Document:\n',result.result);
 
-             dboper.findDocument(db,"dishes",(docs)=>{
+            return dboper.findDocument(db,"dishes")
+})
+.then((docs)=>{
                  console.log("Found Updated Documents:\n",docs);
 
-                 db.dropCollection("dishes",(result)=>{
+            return db.dropCollection("dishes")
+})            
+.then((result)=>{
                      console.log("Droped Collection:",result);
 
                      client.close();
-                 });
-             });
-         });
-     });
-});
-});
+})
+.catch((err)=> console.log(err));      
+})
+.catch((err)=> console.log(err));
     
