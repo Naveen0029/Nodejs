d11b1980a4244bdcce39a962fd33af6e584f7fc3
Naveen Bishnoi <naveendharnia0029@gmail.com>
2020-11-04 12:52:47 +0530

    Mongoose Part-2

--- NodeJs/node-mangoose/index.js
+++ NodeJs/node-mangoose/index.js
@@ -8,26 +8,25 @@ const connect= mongoose.connect(url);
     connect.then((db)=> {
     console.log('Connected correctly to server');
 
-    var newDish = Dishes({
-       name:"Uthappizza",
-       description:'test'
+    Dishes.create({
+        name:'Uthapizza',
+        description:'Test'
+    })
+    .then((dish)=>{
+        console.log(dish);
+
+        return Dishes.find({}).exec();
+    })
+    .then((dishes)=>{
+        console.log(dishes);
+        
+        return Dishes.remove({});
+    })
+    .then(()=>{
+        return mongoose.connection.close();
+    })
+    .catch((err)=>{
+         console.log(err);
     });
-
-    newDish.save()
-        .then((dish)=>{
-            console.log(dish);
-
-            return Dishes.find({}).exec();
-        })
-        .then((dishes)=>{
-            console.log(dishes);
-
-            return Dishes.remove({});
-        })
-        .then(()=>{
-            return mongoose.connection.close();
-        })
-        .catch((err)=>{
-             console.log(err);
-        });
+       
 });
