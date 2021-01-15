4bfa45a4af6ba9c62fcb21e74c5346ea3b943f86
Naveen Bishnoi <naveendharnia0029@gmail.com>
2021-01-16 00:22:28 +0530

    Passport

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 
  
@@ -51,27 +53,23 @@ app.use(session({
 
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth (req, res, next) {
-  console.log(req.session);
+  console.log(req.user);
 
-if(!req.session.user) {
-    var err = new Error('You are not authenticated!');
-    err.status = 403;
-    return next(err);
-}
-else {
-  if (req.session.user === 'authenticated') {
-    next();
-  }
-  else {
-    var err = new Error('You are not authenticated!');
-    err.status = 403;
-    return next(err);
-  }
-}
+    if (!req.user) {
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
+      next(err);
+    }
+    else {
+          next();
+    }
 }
 
   app.use(auth);
--- models/user.js
+++ models/user.js
@@ -1,21 +1,13 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var passportLocalMongoose=require('passport-local-mongoose');
 
-
-var User = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password:  {
-        type: String,
-        required: true
-    },
-    admin:   {
-        type: Boolean,
-        default: false
-    }
+var User=new Schema({
+    admin:{
+        type:Boolean,
+        dafault:false
+    } 
 });
+User.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', User);
